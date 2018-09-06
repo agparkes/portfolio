437721791d75e8754a98bf6dc47ab8591b538af6
agparkes <agparkes413@gmail.com>
2018-09-06 12:26:48 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+// script.js
+
+//starter slides
+ $(function() {
+     
+     $('.slider1').slider({
+  mode: 'horizontal',
+  useCSS: false,
+  infiniteLoop: false,
+  hideControlOnEnd: true,
+  easing: 'easeOutElastic',
+  speed: 2000
+});
+
+     //$('.slider1').bxSlider({
+	//			mode: 'fade',
+	//			captions: false,
+	//			auto:true,
+	//			pager:false,
+				
+	//		});
+	//		$('.slider2').bxSlider({
+	//			pager:false,
+	//			captions: true,
+	//			maxSlides: 3,
+	//			minSlides: 1,
+	//			slideWidth: 230,
+	//			slideMargin: 10
+	//		});
+	//		$('.slider3').bxSlider({
+	//			mode: 'fade',
+	//			captions: false,
+	//			auto:true,
+	//			pager:false,
+	//			controls:false,
+	//		});   
+     
+    // DATEPICKER
+    // https://jqueryui.com/datepicker/
+    
+    // $('.date').datepicker({
+    //    showOtherMonths: true,
+    //    selectOtherMonths: true,
+    //    showButtonPanel: true,
+    //    changeMonth: false,
+    //    changeYear: false,
+    //    numberOfMonths: 2,
+    //    minDate: -1,
+    //    maxDate: "+1W +5D"
+  //  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
